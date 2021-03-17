6cd1d57cd72b3996d5e343440779dc6b9fe05ce8
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-03-17 15:09:19 -0700

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+const h2 = document.createElement('h2');
+h2.textContent = "This is a content added by Javascript";
+document.querySelector('body').appendChild(h2);
+
+const p = document.createElement('p');
+p.textContent = "I am a cool paragraph";
+document.querySelector('body').appendChild(p);
+
